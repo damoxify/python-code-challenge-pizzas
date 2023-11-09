2a90864d759981ad0f2b24a247d1653ea765ef9b
Adedamola Ajasa <azeemajasa@gmail.com>
2023-11-09 17:05:04 +0100

    updated models

--- app/models.py
+++ app/models.py
@@ -1,10 +1,42 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship, backref, ForeignKey
 
 db = SQLAlchemy()
 
 class Restaurant(db.Model):
     __tablename__ = 'Restaurant'
-
+    
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    address = db.Column(db.String)
+    pizzas = db.relationship('Restaurant', backref='pizza')
+
+
+    def __repr__(self):
+        return f'<Restaurant {self.name} is the best in {self.address}>'
 
 # add any models you may need. 
+
+class Pizza(db.Model):
+    __tablename__: 'Pizza'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    ingredients = db.Column(db.String)
+
+
+    def __repr__(self):
+        return f'<Pizza {self.name} is the best in {self.address}>'
+
+
+
+class RestaurantPizza(db.Model):
+    __tablename__: "RestaurantPizza"
+
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Integer)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizza.id'))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'))
+
+    def __repr__(self):
+        return f'<Restaurant_Pizza price is {self.price}>'
